f32d64817bdce38129d5cce5f71776a9f130ebe3
colby <colbykrawczyk10@gmail.com>
2022-07-20 23:36:34 -0400

    Allows custom items to be renamed in an anvil

--- src/main/java/com/github/firewolf8385/customitemapi/listeners/PrepareResultListener.java
+++ src/main/java/com/github/firewolf8385/customitemapi/listeners/PrepareResultListener.java
@@ -3,6 +3,8 @@ package com.github.firewolf8385.customitemapi.listeners;
 import com.destroystokyo.paper.event.inventory.PrepareResultEvent;
 import com.github.firewolf8385.customitemapi.CustomItemAPI;
 import com.github.firewolf8385.customitemapi.items.CustomItem;
+import com.github.firewolf8385.customitemapi.utils.items.ItemBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -31,7 +33,16 @@ public class PrepareResultListener implements Listener {
             return;
         }
 
+
         CustomItem customItem = CustomItemAPI.fromItemStack(item);
-        event.setResult(customItem.update(item));
+
+        if(ChatColor.stripColor(meta.getDisplayName()) != ChatColor.stripColor(customItem.toItemStack().getItemMeta().getDisplayName())) {
+            ItemBuilder builder = new ItemBuilder(item)
+                    .setPersistentData("ci-display_name", ChatColor.stripColor(meta.getDisplayName()));
+            event.setResult(customItem.update(builder.build()));
+        }
+        else {
+            event.setResult(customItem.update(item));
+        }
     }
 }
