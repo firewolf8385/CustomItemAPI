d063b7e19f0890eecbb41160cfce4e75cd710e02
colby <colbykrawczyk10@gmail.com>
2023-01-27 01:43:46 -0500

    Add the "%" character to Crit Chance attribute lore

--- src/main/java/com/github/firewolf8385/customitemapi/items/attributes/attributes/CritChanceAttribute.java
+++ src/main/java/com/github/firewolf8385/customitemapi/items/attributes/attributes/CritChanceAttribute.java
@@ -8,4 +8,33 @@ public class CritChanceAttribute extends ItemAttribute {
         super("crit_chance", "Crit Chance");
     }
 
+    @Override
+    /**
+     * Get the item lore that should be displayed when the attribute has a value.
+     * @param value Value the attribute has on the item.
+     * @return Lore that should be displayed.
+     */
+    public String toLore(double value) {
+        String lore;
+
+        if(Math.floor(value) == value) {
+            if(value > 0) {
+                lore = "&7" + getName() + ": &a+" + ((int) value) + "%";
+            }
+            else {
+                lore = "&7" + getName() + ": &c-" + ((int) value) + "%";
+            }
+        }
+        else {
+            if(value > 0) {
+                lore = "&7" + getName() + ": &a+" + value + "%";
+            }
+            else {
+                lore = "&7" + getName() + ": &c-" + value + "%";
+            }
+        }
+
+        return lore;
+    }
+
 }
