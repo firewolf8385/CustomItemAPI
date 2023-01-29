8bf9db328d93dc25092c3aa91017262680fbf85e
colby <colbykrawczyk10@gmail.com>
2023-01-28 22:43:44 -0500

    Some small cleanup to CustomEnchantment.java. Needs more work

--- src/main/java/com/github/firewolf8385/customitemapi/enchantments/CustomEnchantment.java
+++ src/main/java/com/github/firewolf8385/customitemapi/enchantments/CustomEnchantment.java
@@ -17,13 +17,18 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 public class CustomEnchantment extends Enchantment {
-    private String id;
-    private String name;
+    private final String id;
+    private final String name;
     private int maxLevel;
     private EnchantmentTarget target;
     private EnchantmentRarity rarity;
     private String description;
 
+    /**
+     * Creates a Custom Enchantment
+     * @param id Id of the Custom Enchantment
+     * @param name Name of the Custom Enchantment.
+     */
     public CustomEnchantment(String id, String name) {
         super(new NamespacedKey(Bukkit.getPluginManager().getPlugin("CustomItemAPI"), id));
 
@@ -37,24 +42,49 @@ public class CustomEnchantment extends Enchantment {
         description = "";
     }
 
+
+    /**
+     * Get the description of the enchantment.
+     * Returns empty if there is no description.
+     * @return Enchantment description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Get the id of the enchantment.
+     * @return Enchantment ID.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Change the description of the enchantment.
+     * @param description Enchantment description.
+     * @return Instance of the Custom Enchantment.
+     */
     public CustomEnchantment setDescription(String description) {
         this.description = description;
         return this;
     }
 
+    /**
+     * Sets the enchantment target of the enchantment.
+     * @param target Enchantment Target
+     * @return Custom Enchantment.
+     */
     public CustomEnchantment setTarget(EnchantmentTarget target) {
         this.target = target;
         return this;
     }
 
+    /**
+     * Sets the maximum level of the enchantment.
+     * @param maxLevel Maximum level.
+     * @return CustomEnchantment.
+     */
     public CustomEnchantment setMaxLevel(int maxLevel) {
         this.maxLevel = maxLevel;
         return this;
